1eb33cae628d652de30c4ad6e18e85fab69961de
maniamartial <martialmania19@gmail.com>
2022-08-01 19:00:37 +0300

    UUsing struct

--- MyContract.sol
+++ MyContract.sol
@@ -50,3 +50,44 @@ contract MyContract{
         return state== State.Active;
     }
 }
+
+//simple arecord name sof people
+
+
+
+contract MyContract{
+  Person []public people;
+ uint256 public peopleCount;
+  struct Person{
+      string _firstName;
+      string _lastName;
+  }
+
+  function addPerson(string memory _firstName, string memory _lastName) public{
+     people.push(Person(_firstName, _lastName));
+     peopleCount+=1;
+}
+}
+
+
+//Alternative code of adding and counting peopel using mapping
+
+/*
+
+contract MyContract{
+    uint256 public peopleCount=0;
+//  Person []public people;
+ mapping(uint=>Person) public people;
+  struct Person{
+      uint id;
+      string _firstName;
+      string _lastName;
+  }
+
+  function addPerson(string memory _firstName, string memory _lastName) public{
+         peopleCount+=1;
+     //people.push(Person(_firstName, _lastName));
+people[peopleCount]=Person(peopleCount, _firstName, _lastName);
+}
+}
+*/
