1c57fea242db1ceb5d0d2d0bb363fb828c848d2a
maniamartial <martialmania19@gmail.com>
2022-08-02 17:29:49 +0300

    Using inheritance of contracts

--- MyContract.sol
+++ MyContract.sol
@@ -221,3 +221,76 @@ balances[msg.sender]+=1;
         emit Purchase(msg.sender,1);
     }
 }
+
+//// SPDX-License-Identifier: UNLICENSED
+//using multiple smart contracts..parent child relationships
+
+pragma solidity ^0.5.1;
+contract ERC20Token{
+    string public name;
+    mapping(address=>uint256) public balances;
+
+    function mint() public{
+        balances[tx.origin]++;
+    }
+}
+contract myContract{
+mapping(address=>uint256) public balances;
+address payable wallet;
+address public token;
+
+
+constructor(address payable _wallet, address _token) public{
+wallet=_wallet;
+token=_token;
+}
+
+function() external payable{
+    buyToken();
+}
+
+
+    function buyToken() public payable{
+     //ERC20Token _token=ERC20Token(address(token));
+       // _token.mint();...lets use a nice shorthamd of writing imilar code
+       ERC20Token(address(token)).mint();
+        //buy token
+
+        //send ether to wallet
+        wallet.transfer(msg.value);
+
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+//Inheritance
+pragma solidity ^0.5.1;
+contract ERC20Token{
+    string public name;
+    mapping(address=>uint256) public balances;
+
+constructor(string memory _name) public{
+    name=_name;
+}
+    function mint() public{
+        balances[tx.origin]++;
+    }
+}
+contract myToken is ERC20Token{
+    string public symbol;
+    address[] public owners;
+    uint256 ownerCount;
+
+constructor(string memory _name,
+ string memory _symbol) ERC20Token(_name)
+  public{
+    symbol=_symbol;
+
+}
+
+function mint() public{
+    super.mint();
+    ownerCount++;
+    owners.push(msg.sender);
+}
+}
